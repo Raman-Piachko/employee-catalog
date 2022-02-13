ec907fc9e8b6f730dbeae3a537b4aaa05dd415d2
Raman-Piachko <rpechko88@gmail.com>
2022-02-13 15:21:56 +0300

    done

--- src/main/java/com/epam/rd/autotasks/springemployeecatalog/extractor/EmployeeWithChainExtractor.java
+++ src/main/java/com/epam/rd/autotasks/springemployeecatalog/extractor/EmployeeWithChainExtractor.java
@@ -16,12 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.DEPARTMENT;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.EMPLOYEE_FIRSTNAME;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.EMPLOYEE_HIREDATE;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.FIRSTNAME;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.HIREDATE;
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.ID;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.EMPLOYEE_LASTNAME;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.EMPLOYEE_MANAGER;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.EMPLOYEE_MIDDLENAME;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.LASTNAME;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.MANAGER;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.MIDDLENAME;
 import static java.sql.Types.NULL;
 
 @Component
@@ -38,17 +38,17 @@ public class EmployeeWithChainExtractor implements ResultSetExtractor<List<Emplo
 
     public static Employee createEmployeeWithManager(ResultSet resultSet, Employee manager) throws SQLException {
         Long id = resultSet.getLong(ID.getColumnName());
-        String firstName = resultSet.getString(EMPLOYEE_FIRSTNAME.getColumnName());
-        String lastName = resultSet.getString(EMPLOYEE_LASTNAME.getColumnName());
-        String middleName = resultSet.getString(EMPLOYEE_MIDDLENAME.getColumnName());
+        String firstName = resultSet.getString(FIRSTNAME.getColumnName());
+        String lastName = resultSet.getString(LASTNAME.getColumnName());
+        String middleName = resultSet.getString(MIDDLENAME.getColumnName());
         FullName fullName = new FullName(firstName, lastName, middleName);
-        Position position = Position.valueOf(resultSet.getString(TableColumnNamesEnum.EMPLOYEE_POSITION.getColumnName()));
-        LocalDate hired = resultSet.getDate(String.valueOf(EMPLOYEE_HIREDATE.getColumnName())).toLocalDate();
-        BigDecimal salary = resultSet.getBigDecimal(TableColumnNamesEnum.EMPLOYEE_SALARY.getColumnName());
+        Position position = Position.valueOf(resultSet.getString(TableColumnNamesEnum.POSITION.getColumnName()));
+        LocalDate hired = resultSet.getDate(String.valueOf(HIREDATE.getColumnName())).toLocalDate();
+        BigDecimal salary = resultSet.getBigDecimal(TableColumnNamesEnum.SALARY.getColumnName());
         long departmentId = resultSet.getLong(DEPARTMENT.getColumnName());
         String name = resultSet.getString(TableColumnNamesEnum.NAME.getColumnName());
         String location = resultSet.getString(TableColumnNamesEnum.LOCATION.getColumnName());
-        Employee mgr = resultSet.getLong(TableColumnNamesEnum.EMPLOYEE_MANAGER.getColumnName()) == NULL ? null : manager;
+        Employee mgr = resultSet.getLong(MANAGER.getColumnName()) == NULL ? null : manager;
         Department department = departmentId != 0L ? new Department(departmentId, name, location) : null;
 
         return new Employee(id, fullName, position, hired, salary, mgr, department);
@@ -56,7 +56,7 @@ public class EmployeeWithChainExtractor implements ResultSetExtractor<List<Emplo
 
     public static Employee getManagersManager(ResultSet resultSet) throws SQLException {
         int currentRow = resultSet.getRow();
-        long managerId = resultSet.getLong(EMPLOYEE_MANAGER.getColumnName());
+        long managerId = resultSet.getLong(MANAGER.getColumnName());
         resultSet.beforeFirst();
         Employee manager = null;
         while (resultSet.next()) {
--- src/main/java/com/epam/rd/autotasks/springemployeecatalog/extractor/SimpleEmployeeExtractor.java
+++ src/main/java/com/epam/rd/autotasks/springemployeecatalog/extractor/SimpleEmployeeExtractor.java
@@ -1,6 +1,5 @@
 package com.epam.rd.autotasks.springemployeecatalog.extractor;
 
-import com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum;
 import com.epam.rd.autotasks.springemployeecatalog.domain.Department;
 import com.epam.rd.autotasks.springemployeecatalog.domain.Employee;
 import com.epam.rd.autotasks.springemployeecatalog.domain.FullName;
@@ -20,14 +19,18 @@ import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNa
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.ID;
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.LASTNAME;
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.LOCATION;
-import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.MANAGER_HIREDATE;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.MANAGER;
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.MIDDLENAME;
 import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.NAME;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.POSITION;
+import static com.epam.rd.autotasks.springemployeecatalog.constant.TableColumnNamesEnum.SALARY;
 
 @Component
 public class SimpleEmployeeExtractor implements ResultSetExtractor<List<Employee>> {
-    private final static String D_E = "_d_e";
-    private final static String D_M = "_d_m";
+    private final static String SUFFIX_D_E = "_d_e";
+    private final static String SUFFIX_D_M = "_d_m";
+    private final static String SUFFIX_E = "_e";
+    private final static String SUFFIX_M = "_m";
 
     @Override
     public List<Employee> extractData(ResultSet resultSet) throws SQLException {
@@ -40,33 +43,33 @@ public class SimpleEmployeeExtractor implements ResultSetExtractor<List<Employee
     }
 
     public static Employee createEmployeeWithManager(ResultSet resultSet) throws SQLException {
-        Long id = resultSet.getLong(TableColumnNamesEnum.ID_E.getColumnName());
-        String firstName = resultSet.getString(FIRSTNAME.getColumnName());
-        String lastName = resultSet.getString(LASTNAME.getColumnName());
-        String middleName = resultSet.getString(MIDDLENAME.getColumnName());
+        Long id = resultSet.getLong(ID.getColumnName() + SUFFIX_E);
+        String firstName = resultSet.getString(FIRSTNAME.getColumnName() + SUFFIX_E);
+        String lastName = resultSet.getString(LASTNAME.getColumnName() + SUFFIX_E);
+        String middleName = resultSet.getString(MIDDLENAME.getColumnName() + SUFFIX_E);
         FullName fullName = new FullName(firstName, lastName, middleName);
-        Position position = Position.valueOf(resultSet.getString(TableColumnNamesEnum.POSITION.getColumnName()));
-        LocalDate hired = resultSet.getDate(HIREDATE.getColumnName()).toLocalDate();
-        BigDecimal salary = resultSet.getBigDecimal(TableColumnNamesEnum.SALARY.getColumnName());
-        long departmentId = resultSet.getLong(ID.getColumnName() +D_E);
-        String name = resultSet.getString(NAME.getColumnName() + D_E);
-        String location = resultSet.getString(LOCATION.getColumnName() + D_E);
+        Position position = Position.valueOf(resultSet.getString(POSITION.getColumnName() + SUFFIX_E));
+        LocalDate hired = resultSet.getDate(HIREDATE.getColumnName() + SUFFIX_E).toLocalDate();
+        BigDecimal salary = resultSet.getBigDecimal(SALARY.getColumnName() + SUFFIX_E);
+        long departmentId = resultSet.getLong(ID.getColumnName() + SUFFIX_D_E);
+        String name = resultSet.getString(NAME.getColumnName() + SUFFIX_D_E);
+        String location = resultSet.getString(LOCATION.getColumnName() + SUFFIX_D_E);
         Department department = departmentId != 0L ? new Department(departmentId, name, location) : null;
-        Long managerId = resultSet.getLong(TableColumnNamesEnum.MANAGER.getColumnName());
+        Long managerId = resultSet.getLong(MANAGER.getColumnName() + SUFFIX_E);
 
         Employee manager = null;
 
         if (managerId != 0L) {
-            String managerFirstname = resultSet.getString(TableColumnNamesEnum.MANAGER_FIRSTNAME.getColumnName());
-            String managerLastname = resultSet.getString(TableColumnNamesEnum.MANAGER_LASTNAME.getColumnName());
-            String managerMiddlename = resultSet.getString(TableColumnNamesEnum.MANAGER_MIDDLENAME.getColumnName());
+            String managerFirstname = resultSet.getString(FIRSTNAME.getColumnName() + SUFFIX_M);
+            String managerLastname = resultSet.getString(LASTNAME.getColumnName() + SUFFIX_M);
+            String managerMiddlename = resultSet.getString(MIDDLENAME.getColumnName() + SUFFIX_M);
             FullName managerFullName = new FullName(managerFirstname, managerLastname, managerMiddlename);
-            Position managerPosition = Position.valueOf(resultSet.getString(TableColumnNamesEnum.MANAGER_POSITION.getColumnName()));
-            LocalDate managerHired = resultSet.getDate(MANAGER_HIREDATE.getColumnName()).toLocalDate();
-            BigDecimal managerSalary = resultSet.getBigDecimal(TableColumnNamesEnum.MANAGER_SALARY.getColumnName());
-            long managerDepartmentId = resultSet.getLong(ID.getColumnName() + D_M);
-            String managerDepartmentName = resultSet.getString(NAME.getColumnName() + D_M);
-            String managerDepartmentLocation = resultSet.getString(LOCATION.getColumnName() + D_M);
+            Position managerPosition = Position.valueOf(resultSet.getString(POSITION.getColumnName() + SUFFIX_M));
+            LocalDate managerHired = resultSet.getDate(HIREDATE.getColumnName() + SUFFIX_M).toLocalDate();
+            BigDecimal managerSalary = resultSet.getBigDecimal(SALARY.getColumnName() + SUFFIX_M);
+            long managerDepartmentId = resultSet.getLong(ID.getColumnName() + SUFFIX_D_M);
+            String managerDepartmentName = resultSet.getString(NAME.getColumnName() + SUFFIX_D_M);
+            String managerDepartmentLocation = resultSet.getString(LOCATION.getColumnName() + SUFFIX_D_M);
             Department managerDepartment = managerDepartmentId != 0L ?
                     new Department(managerDepartmentId, managerDepartmentName, managerDepartmentLocation) : null;
 
